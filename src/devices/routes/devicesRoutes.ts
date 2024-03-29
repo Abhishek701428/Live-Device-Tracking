80a3059db97d9499ab873e38ff81b85b653d54f3
Khushbu Verma <154433565+khushbuhitech@users.noreply.github.com>
2024-03-29 14:55:10 +0530

    Update devicesRoutes.ts

--- src/devices/routes/devicesRoutes.ts
+++ src/devices/routes/devicesRoutes.ts
@@ -9,22 +9,16 @@ router.get('/devices', authenticateJWT, getAllDevices);
 router.get('/devices/specificData/:id', getSpecificById);
 router.get('/last-data', authenticateJWT, async (req: Request, res: Response) => {
   try {
-    if (!(req as any).user) { 
+    if (!(req as any).user) {
       return res.status(401).json({ error: 'Authentication failed: User not found.' });
     }
-    const userDevices = (req as any).user.devicesId; 
-    const allDevicesLastData = await Promise.all(userDevices.map(async (deviceId: string) => {
-      try {
-        const lastData = await Subscription.findOne({ deviceId }).sort({ nowTime: -1 }).limit(1);
-        return lastData;
-      } catch (error) {
-        console.error(Error fetching last data for device ${deviceId}:, error);
-        return null;
-      }
+    const user = (req as any).user;
+    const userDevices = user.devicesId;
+    const allDevicesLastData = await Promise.all(userDevices.map(async deviceId => {
+      const lastData = await Subscription.aggregate([{ $match: { deviceId: deviceId } }, { $sort: { nowTime: -1 } }, { $limit: 1 }]);
+      return lastData[0];
     }));
-    const filteredData = allDevicesLastData.filter(data => data !== null && data !== undefined);
-
-    res.json(filteredData);
+    res.json(allDevicesLastData.filter(data => data));
   } catch (error) {
     console.error('Error fetching last data for user devices:', error);
     res.status(500).json({ error: 'Internal server error' });
